fcb0fb4a54280ae5fb962840a5cf12f6375094c9
hanopav <hanosekpavel@gmail.com>
2025-01-14 22:50:59 +0100

    added Player class and Triangle/Circle shaped hitbox

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 0 != 1:
+    clock = pygame.time.Clock()
+    dt = 0
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(0, None, 0)
-        """
-        print("Starting asteroids!")
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
-        """
+        screen.fill((0, 0, 0), None, 0)
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
     pygame.display.flip()
 if __name__ == "__main__":
     main()
